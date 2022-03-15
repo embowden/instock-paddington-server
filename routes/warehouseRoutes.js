9505861dfc476e69abd544d1b0cd09dd05bb808c
cupofkoffeeing <97884217+chrismitchelll@users.noreply.github.com>
2022-03-15 22:01:46 +0000

    J2n-11 feature added

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -14,6 +14,7 @@ router.get("/", (req, res) => {
   const warehouseData = readFile("./data/warehouses.json");
   res.status(200).json(warehouseData);
 });
+///////----------------//////////
 ///------GET Individual------///
 router.get("/:id", (req, res) => {
   const warehouseContent = readFile("./data/warehouses.json");
@@ -22,8 +23,6 @@ router.get("/:id", (req, res) => {
   );
   res.status(200).json(selectedWarehouse);
 });
-///---------------------------///
-
 // DELETE WAREHOUSE AND CORRESPONDING INVENTORY
 router.delete("/:warehouseId", (req, res) => {
   const warehouseId = req.params.warehouseId;
@@ -72,38 +71,33 @@ router.post("/", (req, res) => {
     },
   };
 
-
-    warehouseData.push(newWarehouse);
-    fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseData))
-    res.status(201).json(newWarehouse); 
+  warehouseData.push(newWarehouse);
+  fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseData));
+  res.status(201).json(newWarehouse);
 });
 
-
-
 //*EDIT-WAREHOUSE
-router.post("/:warehouseId", (req, res)=> {
-    const warehouseId = req.params.warehouseId;
-    const warehouseData = JSON.parse(fs.readFileSync("./data/warehouses.json"));
+router.post("/:warehouseId", (req, res) => {
+  const warehouseId = req.params.warehouseId;
+  const warehouseData = JSON.parse(fs.readFileSync("./data/warehouses.json"));
 
-    const warehouse = warehouseData.find((object) => object.id == warehouseId);
-        if (!warehouse) {
-        res.status(400).json("Please provide a valid ID");
+  const warehouse = warehouseData.find((object) => object.id == warehouseId);
+  if (!warehouse) {
+    res.status(400).json("Please provide a valid ID");
   }
-    const editedWarehouse = {id: warehouseId, ...req.body}
+  const editedWarehouse = { id: warehouseId, ...req.body };
 
-//* This function finds the index of the desired warehouse
-    const findIndex = () => {
-    for(let i = 0; warehouseData.length; i++ ){
-        if(warehouseData[i].id == warehouseId){
-            return i
-        }
+  //* This function finds the index of the desired warehouse
+  const findIndex = () => {
+    for (let i = 0; warehouseData.length; i++) {
+      if (warehouseData[i].id == warehouseId) {
+        return i;
+      }
     }
-}
+  };
 
-     warehouseData.splice(findIndex(), 1 , editedWarehouse);
-     
-     fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseData))
-     res.status(201).json(warehouseData); 
-    
-})
+  warehouseData.splice(findIndex(), 1, editedWarehouse);
 
+  fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseData));
+  res.status(201).json(warehouseData);
+});
